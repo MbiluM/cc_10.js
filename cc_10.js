f736a05672ec25401fd320057514ec400d5bcd8d
MbiluM <mabilu@usf.edu>
2025-02-23 21:03:05 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -17,11 +17,28 @@ class product {
         }
     }
 }
+
+//Task 2 - Created Order Class
+class Order { 
+    constructor(orderId, product, quantity){
+    this.orderId = orderId
+    this.product = product
+    this.quantity = quantity
+    }
+    getOrderDetails (){
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity},Total Price: $${this.quantity * this.product.price}`;
+    }
+}
+
 // Test Cases
 const prod1 = new product ("Laptop", 101, 1200, 10);
-console.log(prod1.getDetails());
-// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 10"
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 10"
 
 prod1.updateStock(3);
-console.log(prod1.getDetails());
-// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); // Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
+
